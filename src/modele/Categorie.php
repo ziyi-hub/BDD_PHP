0e5c4e512918af1daf4568415375521470e848c7
Ziyi Wang <ziyiwang1027@gmail.com>
2021-03-11 09:49:38 +0100

    Ajout des models

---
+++ src/modele/Categorie.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace td2\modele;
+use Illuminate\Database\Eloquent\Model;
+
+class Categorie extends Model
+{
+    protected $table = 'categorie';
+    protected $primaryKey='idCategorie';
+}
